28cd82b12119a678d6da966482b5f93267373eca
Blair MacIntyre <blair@macmynatt.com>
2021-08-04 20:31:57 -0400

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
         ref: 'origin/main',
         repo: 'git@github.com:realitymediabook/realitymedia-top-level.git',
         path: '/home/ubuntu/server',
-        'post-deploy': 'npm install && git submodule update --init --recursive && pm2 startOrRestart ecosystem.config.js'
+        'post-deploy': 'npm install && git submodule update --init --recursive && (cp ../.env .env || pm2 startOrRestart ecosystem.config.js)'
       }
     }
   }
