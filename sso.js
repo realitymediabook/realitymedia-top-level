7717170e3f87cfce39ae3a9fe7589ae72d1ae2ef
Blair MacIntyre <blair@macmynatt.com>
2021-08-05 10:46:01 -0400

    https

--- sso.js
+++ sso.js
@@ -11,7 +11,7 @@ const DB = require('./db');
 const app = express();
 
 var corsOptions = {
-    origin: 'http://xr.realitymedia.digital',
+    origin: 'https://xr.realitymedia.digital',
 }
 app.use(cors(corsOptions))
 
